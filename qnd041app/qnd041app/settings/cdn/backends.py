0b94819cbef1fe2208cf4f9d3b7f50b8c5c639cc
smartquailstage <smartquail.stage@gmail.com>
2025-08-05 14:07:47 -0500

    dsf

--- qnd041app/qnd041app/settings/cdn/backends.py
+++ qnd041app/qnd041app/settings/cdn/backends.py
@@ -1,9 +1,18 @@
 from storages.backends.s3boto3 import S3Boto3Storage
 
+
+aws_location = os.environ.get("AWS_LOCATION", "qn041app")
+
+
 class StaticRootS3BotoStorage(S3Boto3Storage):
-    location = "static"
-    default_acl = 'public-read'  # ✅ Asegura que los archivos estáticos sean accesibles públicamente
+    location = f"{aws_location}/static"
+    default_acl = 'public-read'  
+    file_overwrite = False
 
 class MediaRootS3BotoStorage(S3Boto3Storage):
-    location = "media"
+    location = f"{aws_location}/media"
     default_acl = 'public-read'  # ✅ Cambia a 'private' si quieres proteger los archivos multimedia
+    file_overwrite = False
+
+
+
