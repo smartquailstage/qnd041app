6bcaac1dfa7768bc6762b34806705ce962b270ae
smartquailstage <smartquail.stage@gmail.com>
2025-11-03 22:16:08 -0500

    asa

--- qnd041app/usuarios/tasks.py
+++ qnd041app/usuarios/tasks.py
@@ -13,6 +13,8 @@ from email.utils import formataddr
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
+from django.urls import reverse
+from django.template.loader import render_to_string
 
 
 from django.utils.html import strip_tags
