ca15ea3c918e8483e8b6ded58ba418e4be9bcdfe
smartquailstage <smartquail.stage@gmail.com>
2026-01-11 21:21:22 -0500

    qwr

--- qnd041app/saas_payment/urls.py
+++ qnd041app/saas_payment/urls.py
@@ -6,7 +6,7 @@ from django.utils.translation import gettext_lazy as _
 app_name = 'saas_payment'
 
 urlpatterns = [
-    path(_('process/'), views.payment_process, name='process'),
+    path(_('process/'), views.payment_process, name='payment_process_kushki'),
     path(_('done/'), views.payment_done, name='done'),
     path(_('canceled/'), views.payment_canceled, name='canceled'),
 ]
