04208fae4d24913b2cfc721732ac0425ad358361
smartquailstage <smartquail.stage@gmail.com>
2026-02-10 00:43:00 -0500

    sss

--- qnd041app/smartbusinessanalytics_id/apps.py
+++ qnd041app/smartbusinessanalytics_id/apps.py
@@ -4,6 +4,7 @@ class SmartbusinessanalyticsIdConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     # Debe ser el nombre completo del módulo Python
     name = 'smartbusinessanalytics_id'
+    verbose_name = 'SmartBusinessAnalytics® (I+D)'
 
     def ready(self):
         import smartbusinessanalytics_id.signals
