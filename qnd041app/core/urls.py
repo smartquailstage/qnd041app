22dc0644e0d6f3eebdc9698d23c231136f7068dd
smartquailstage <smartquail.stage@gmail.com>
2026-02-26 00:55:48 -0500

    Update urls.py

--- qnd041app/core/urls.py
+++ qnd041app/core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import social_callback
+from .views import social_callback,update_generated_image
 
 urlpatterns = [
     path("social/callback/", social_callback, name="social_callback"),
-    path("api/update_image_url/", views.update_generated_image, name="update_generated_image"),
+    path("api/update_image_url/", update_generated_image, name="update_generated_image"),
 ]
 
