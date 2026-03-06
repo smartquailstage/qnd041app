7b47ebf337386ca72149d8905458856aec038b28
smartquailstage <smartquail.stage@gmail.com>
2026-03-06 16:19:45 -0500

    ada

--- qnd041app/core/urls.py
+++ qnd041app/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import social_callback,update_generated_image,update_generated_video
+from .views import social_callback,update_generated_image,update_generated_video,update_carousel_generated_images
 
 app_name = 'core'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path("social/callback/", social_callback, name="social_callback"),
     path("api/update_image_url/", update_generated_image, name="update_generated_image"),
     path("api/update_video_url/", update_generated_video, name="update_generated_video"),
+    path("api/update_carousel_images/", update_carousel_generated_images, name="update_carousel_generated_images"),
 ]
