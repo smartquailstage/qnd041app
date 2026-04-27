2c6ced1c8322dc08348930d81ce73e8b8a8d3945
smartquailstage <smartquail.stage@gmail.com>
2026-04-27 01:27:17 -0500

    Update urls.py

--- qnd041app/social_media_AI/urls.py
+++ qnd041app/social_media_AI/urls.py
@@ -1,12 +1,29 @@
 from django.urls import path
-from .views import SaveGeneratedImageView,InstagramWebhookView,GenericWebhookCallbackView
 
+from .views import (
+    SaveGeneratedImageView,
+    InstagramWebhookView,
+    GenericWebhookCallbackView
+)
 
-app_name = 'social_media_AI'
-
+app_name = "social_media_AI"
 
 urlpatterns = [
-    path("webhooks/wagtail/save-image/", SaveGeneratedImageView),
-    path("webhooks/n8n/instagram/post", InstagramWebhookView, name="n8n-instagram-post-webhook"),
-    path("webhooks/n8n/", GenericWebhookCallbackView, name="n8n-webhook"),
+    path(
+        "webhooks/wagtail/save-image/",
+        SaveGeneratedImageView.as_view(),
+        name="save-image"
+    ),
+
+    path(
+        "webhooks/n8n/instagram/post",
+        InstagramWebhookView.as_view(),
+        name="n8n-instagram-post-webhook"
+    ),
+
+    path(
+        "webhooks/n8n/",
+        GenericWebhookCallbackView.as_view(),
+        name="n8n-webhook"
+    ),
 ]
