c047f1d3add059329358bffded8b95fecab0dbd4
smartquailstage <smartquail.stage@gmail.com>
2026-04-27 00:53:45 -0500

    Update urls.py

--- qnd041app/social_media_AI/urls.py
+++ qnd041app/social_media_AI/urls.py
@@ -6,7 +6,7 @@ app_name = 'social_media_AI'
 
 
 urlpatterns = [
-    path("webhooks/wagtail/save-image/", SaveGeneratedImageView.as_view()),
-    path("webhooks/n8n/instagram/post", InstagramWebhookView.as_view(), name="n8n-instagram-post-webhook"),
-    path("webhooks/n8n/", GenericWebhookCallbackView.as_view(), name="n8n-webhook"),
+    path("webhooks/wagtail/save-image/", SaveGeneratedImageView),
+    path("webhooks/n8n/instagram/post", InstagramWebhookView, name="n8n-instagram-post-webhook"),
+    path("webhooks/n8n/", GenericWebhookCallbackView, name="n8n-webhook"),
 ]
