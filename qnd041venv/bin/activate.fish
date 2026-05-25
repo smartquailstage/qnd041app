c0e9e28fdf716ed3f324fb2a63493c2a181fa03f
smartquailstage <smartquail.stage@gmail.com>
2026-05-25 17:01:47 -0500

    dsad

--- qnd041venv/bin/activate.fish
+++ qnd041venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /Users/smartquail/Desktop/qnd041app/qnd041venv
+set -gx VIRTUAL_ENV /Users/smartquail/Desktop/QNODES2025/QND0/qnd041app/qnd041venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
