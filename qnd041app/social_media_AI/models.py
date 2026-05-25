6e96350ba3e8f2bb130b7b4f79fd0530eb7a2e5a
smartquailstage <smartquail.stage@gmail.com>
2026-05-24 19:38:06 -0500

    nmmn

--- qnd041app/social_media_AI/models.py
+++ qnd041app/social_media_AI/models.py
@@ -175,12 +175,14 @@ class InstagramPost(BasePost):
                 FieldPanel("post_type", classname="col6"),
                 FieldPanel("image_size", classname="col6"),
                 FieldPanel("scheduled_date", classname="col6"),
+                FieldPanel("caption", classname="col6"),
+                FieldPanel("copy", classname="col6"),
+                FieldPanel("hashtags", classname="col6"),
             ]),
         ], heading="Configure su Instagram Post"),
         
         FieldPanel("prompt"),
     ]
-
 
 
     class Meta:
